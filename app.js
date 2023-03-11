b6e18a2e5c4094e8bf8002806bd1113cb4fcb5b7
audreyqiu2 <audreyqiu2@gmail.com>
2023-03-10 16:04:00 -0800

    pushing

---
+++ app.js
@@ -0,0 +1,209 @@
+// imports
+var SpotifyWebApi = require('spotify-web-api-node')
+const express = require("express");
+const jwt = require('jsonwebtoken');
+// const decodedToken = jwt.decode(access_token)
+const axios = require('axios');
+const request = require("request");
+const { text } = require('express');
+const app = express();
+app.use(express.json());
+
+const port = null;
+if (window.location.href == "http://localhost:4000/callback") {
+  port = 4000;
+} else {
+  port = 443;
+}
+
+
+var CLIENT_ID = '9d94f606e5f14c6e9d25d87b2ed63cfb';
+var CLIENT_SECRET = '2598aa227a584e9e8ca50b0ff559d01c';
+var REDIRECT_URI = window.location.href + "callback";
+
+// Scope of the API calls
+const scope = "user-modify-playback-state streaming user-library-read playlist-read-private";
+
+
+// Base URL for making calls to Spotify API once logged in
+const BASE_URL = "https://api.spotify.com/v1";
+
+// access code + token
+let code = "";
+let access_token = "";
+let refresh_token = "";
+let expires_at = null;
+
+// static files
+app.use(express.static("public"));
+app.use("/styles.css", express.static(__dirname + "/public/styles.css"));
+app.use("/index.js", express.static(__dirname + "/public/index.js"));
+app.use("/index.html", express.static(__dirname + "/public/index.html"));
+
+// Request access token and refresh token from Spotify API
+app.get('/callback', (req, res) => {
+  var siteUrl = req.protocol + "://" + req.get('host');
+  console.log(siteUrl);
+
+  const code = req.query.code;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: REDIRECT_URI,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+    },
+    json: true
+  };
+
+  request.post(authOptions, (error, response, body) => {
+    access_token = body.access_token;
+    refresh_token = body.refresh_token;
+
+    console.log('access token', access_token);
+    console.log('refresh token', refresh_token);
+
+    res.append('Set-Cookie', 'access_token=' + access_token);
+    res.sendFile(__dirname + "/public/index.html");
+  });
+});
+
+// Gets basic current user information
+app.get('/get-me', (req, res) => {
+  const authOptions = {
+    url: BASE_URL + '/me',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${access_token}`
+    }
+  };
+
+  axios.get(authOptions.url, { headers: authOptions.headers })
+    .then(response => {
+      // console.log(response.data);
+      res.status(200).send(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.type(text);
+      res.status(500).send('Error getting user info');
+    })
+});
+
+// Gets current user's top playlists
+app.get('/get-my-playlists', (req, res) => {
+  const authOptions = {
+    url: BASE_URL + '/me/playlists?limit=10',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${access_token}`
+    }
+  };
+
+  axios.get(authOptions.url, { headers: authOptions.headers })
+    .then(response => {
+      // console.log(response.data);
+      res.status(200).send(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.type(text);
+      res.status(500).send('Error getting users playlists');
+    })
+});
+
+app.get('/get-devices', async (req,res) => {
+  const playlistUrl = req.body.playlist_url;
+  console.log(playlistUrl);
+  let url = BASE_URL + "/me/player/devices";
+
+  try {
+    const response = await axios.put(url, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+});
+
+// Start Spotify playback
+app.put('/playback/start', async (req, res) => {
+  const playlistUri = req.body.playlist_uri;
+  console.log(playlistUri);
+  let url = BASE_URL + "/me/player/play";
+
+  try {
+    const response = await axios.put(url,
+    {
+      context_uri: playlistUri
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+
+    res.status(200).send('Playback started successfully');
+  } catch (error) {
+    // console.log(error);
+    res.status(500);
+  }
+});
+
+// Get more playlist info
+app.post('/get-playlist-info', async (req, res) => {
+  const playlistUrl = req.body.playlist_url;
+  console.log(playlistUrl);
+  let url = BASE_URL + "/me/player/play";
+  // url += "";
+  // console.log(url);
+  // let filter = "fields=tracks.items(added_at)";
+
+  try {
+    const response = await axios.put(url, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Something went wrong!');
+  }
+});
+
+// Uses the refresh token to ask for a new access token
+app.get('/refresh_token', function(req, res) {
+  console.log('refresh token:', refresh_token);
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      access_token = body.access_token;
+      console.log('refreshed access token:', access_token);
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+})
+
+app.listen(port, () => console.info("listening on port ${port}"));
---
+++ public/visualizer.js
@@ -0,0 +1,87 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Uses the p5 sound library to implement the audiovisualizer                                     //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let song1;
+// let song2;
+// let song3;
+let fft;
+let mic;
+
+let w;
+
+// function preload() {
+//   soundFormats('mp3');
+//   // song1 = loadSound('assets/better_off_alone.mp3');
+//   // song2 = loadSound('assets/island.mp3');
+//   // song3 = loadSound('assets/endlessly.mp3');
+// }
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  window.parent.postMessage({type: 'resize', width: width, height: height}, '*');
+
+  mic = new p5.AudioIn();
+  mic.start();
+
+  angleMode(DEGREES);
+  fft = new p5.FFT(0.9, 512);
+  fft.setInput(mic);
+  w = windowWidth / 512;
+}
+
+// On window resize, update the canvas size
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+  w = windowWidth / 512;
+}
+
+function keyTyped() {
+}
+
+function draw() {
+  colorMode(RGB);
+  background(34, 34, 34);
+  let spectrum = fft.analyze();
+  let waves = fft.waveform();
+  noStroke();
+  for (let i = 0; i < spectrum.length; i++) {
+    let amp = spectrum[i];
+    let energy = fft.getEnergy(i);
+    let newWaveForm = map(waves[i], -1, 1, 0, 255);
+    let y = map(amp, 0, spectrum.length, windowHeight, 0);
+    let x = map(i, 0, spectrum.length, i, windowWidth * w);
+
+    let ampMeasure = map(amp, 0, spectrum.length, 0, 255);
+
+    ////////////////////////////
+    // DIFFERENT COLOR THEMES //
+    ////////////////////////////
+
+    // LESS STROBE-Y
+    // More blue-centric color theme
+    // fill(255 - energy, newWaveForm, energy);
+
+    // More purple-centric color theme
+    // fill(newWaveForm, 255 - energy, energy);
+
+    // MORE STROBE-Y
+    // More strobe-y purple-centric color theme
+    // fill(newWaveForm, 255 - energy, ampMeasure);
+
+    // More strobe-y green-centric color theme
+    // fill(ampMeasure, newWaveForm, 255 - energy);
+
+    // !!! Strobe-y orange-green theme !!!
+    fill(newWaveForm, ampMeasure, 255 - energy);
+
+    // Strobe-y light blue-purple theme
+    // fill(newWaveForm, ampMeasure, energy);
+
+    rect(x, y, w, windowHeight - y/4);
+    // rect(x, y, w, y)
+
+    fill(255, 255, 255);
+  }
+}
