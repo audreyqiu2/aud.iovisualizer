fceee58cdf3eab0c46bfad046444c2166f5ea4f4
audreyqiu2 <audreyqiu2@gmail.com>
2023-03-15 17:51:01 -0700

    typo fix

--- public/index.js
+++ public/index.js
@@ -112,7 +112,7 @@ function afterAuthentication() {
 }
 
 function getCookie(name) {
-  conosle.log(document.cookie);
+  console.log(document.cookie);
   const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
   if (match) {
     return match[2];
